dcf80697271781aec2b7a6191450c5ebc9a0a726
supankarchb <supankar710@gmail.com>
2021-09-26 19:20:14 +0600

    allComitted

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
         <div className="border border-3 rounded border-primary bg-dark m-2">
            <div className="text-white "> <h1 className="fw-bold">Grand Openning Ceremony</h1>
             <p><small>This is a grand openning ceremony party.So a company arranged a mindblowing party with great joy.So they hire some actor or actress for attending their openning ceromony party.They also have budget too.</small></p>
-            <h2 className="fw-bold">Total Budget 5 Lack</h2></div>
+            <h2 className="fw-bold">Total Budget 1 Million</h2></div>
         </div>
     );
 };
